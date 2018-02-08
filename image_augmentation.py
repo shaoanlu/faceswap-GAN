b958322e9593ab9c6c4f7823799c0cd4861b760e
shaoanlu <shaoanlu@gmail.com>
2018-02-08 22:52:01 +0800

    add randomwarp224()

--- image_augmentation.py
+++ image_augmentation.py
@@ -72,3 +72,29 @@ def random_warp128(image):
     target_image = cv2.warpAffine(image, mat, (64*2,64*2))
 
     return warped_image, target_image
+
+# get pair of random warped images from aligened face image
+def random_warp224(image):
+    assert image.shape == (256,256,3)
+    range_ = numpy.linspace(128-110, 128+110, 5)
+    mapx = numpy.broadcast_to(range_, (5,5))
+    mapy = mapx.T
+
+    mapx = mapx + numpy.random.normal(size=(5,5), scale=6)
+    mapy = mapy + numpy.random.normal(size=(5,5), scale=6)
+
+    interp_mapx = cv2.resize(mapx, (80*4,80*4))[8*4:72*4,8*4:72*4].astype('float32')
+    interp_mapy = cv2.resize(mapy, (80*4,80*4))[8*4:72*4,8*4:72*4].astype('float32')
+
+    warped_image = cv2.remap(image, interp_mapx, interp_mapy, cv2.INTER_LINEAR)
+
+    src_points = numpy.stack([mapx.ravel(), mapy.ravel() ], axis=-1)
+    dst_points = numpy.mgrid[0:65*4:16*4,0:65*4:16*4].T.reshape(-1,2)
+    mat = umeyama(src_points, dst_points, True)[0:2]
+
+    target_image = cv2.warpAffine(image, mat, (64*4,64*4))
+    
+    target_image = cv2.resize(target_image, (224,224))
+    warped_image = cv2.resize(warped_image, (224,224))
+
+    return warped_image, target_image
